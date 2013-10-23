7cc091e1ad628c08db23a434689fde5ec15882bb
Jan-Oliver Pantel <jan.pantel@gmail.com>
2013-10-23 17:18:21 +0200

    Implemented ResolveCommand function

--- src/Commandr/Commandr.cs
+++ src/Commandr/Commandr.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
+using Commandr.Attributes;
 using Commandr.Shared;
 using Commandr.Utils.CommandSplitter;
 using Commandr.Utils.Output;
@@ -15,6 +16,7 @@ namespace Commandr
         protected ICollection<ICommand> commands;
         protected ICommandSplitter splitter;
         protected IOutput output;
+        protected IDictionary<string, ICommand> cache;
         
         /// <summary>
         /// Creates a new instance of <see cref="Commandr.Commandr"/>.
@@ -23,12 +25,14 @@ namespace Commandr
         public Commandr(
         	ICollection<ICommand> commands = null, 
         	ICommandSplitter splitter = null,
-        	IOutput output = null
+        	IOutput output = null,
+        	IDictionary<string, ICommand> cache = null
         )
         {
         	this.commands = commands ?? new List<ICommand>();
         	this.splitter = splitter ?? new DefaultCommandSplitter();
         	this.output = output ?? new ConsoleOutput();
+        	this.cache = cache ?? new Dictionary<string, ICommand>();
         }
         
         public void RegisterCommand(ICommand cmd)
@@ -38,7 +42,27 @@ namespace Commandr
         
         public void ResolveCommand(string cmd)
         {
-        	        	
+        	var splitted = this.splitter.SplitCommand(cmd);
+        	
+        	var command = 
+        		(from item in this.commands
+        		let type = item.GetType()
+        		let commandAttribute = 
+        			type.GetCustomAttributes(typeof(CommandAttribute), true).FirstOrDefault()
+        		where commandAttribute != null
+        		&& (commandAttribute as CommandAttribute).Command == splitted.Command
+        		select item).FirstOrDefault();
+        	
+        	if (command == null)
+        	{
+        		throw new ArgumentException("The command " + splitted.Command + " does not exist");
+        	}
+        	
+        	this.cache.Add(splitted.Command, command);
+        	
+        	var message = command.Run(splitted.Arguments);
+        	
+        	message.ToList().ForEach(x => this.output.Write(x));
         }
     }
 }

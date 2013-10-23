36d10aef764fdc2ad2c0a6cce09aac2e795f1f7a
Jan Pantel <jan.pantel@gmail.com>
2013-10-23 10:22:06 +0200

    Added Commandr class

---
+++ src/Commandr/Commandr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Commandr
+{
+    public class Commandr
+    {
+        protected IEnumerable<_Assembly> assemblies;
+
+        public Commandr(IEnumerable<_Assembly> assemblies = null)
+        {
+            this.assemblies = assemblies ?? new _Assembly[] { Assembly.GetCallingAssembly() };
+        }
+    }
+}

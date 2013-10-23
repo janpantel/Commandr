8d1bfdb55cd681ee861ed0c0904521d6711413d6
Jan Pantel <jan.pantel@gmail.com>
2013-10-23 10:38:59 +0200

    Added constructors to Commandr class and added ICommand interface

--- src/Commandr/Commandr.cs
+++ src/Commandr/Commandr.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Commandr.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -6,11 +9,39 @@ namespace Commandr
 {
     public class Commandr
     {
-        protected IEnumerable<_Assembly> assemblies;
+        protected IEnumerable<_Type> commands;
 
-        public Commandr(IEnumerable<_Assembly> assemblies = null)
+        /// <summary>
+        /// Creates a new instance of <see cref="Commandr.Commandr"/> with the commands from the calling assembly.
+        /// </summary>
+        public Commandr() : this(new List<_Assembly>() { Assembly.GetCallingAssembly() })
         {
-            this.assemblies = assemblies ?? new _Assembly[] { Assembly.GetCallingAssembly() };
+            
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Commandr.Commandr"/> with the commands from the given
+        /// assemblies.
+        /// If
+        /// </summary>
+        /// <param name="assemblies">A collection of assemblies from where Commandr should load the command classes.</param>
+        public Commandr(IEnumerable<_Assembly> assemblies)
+        {
+            this.commands = 
+                //Select every type from the assemblies...
+                assemblies.SelectMany(a => a.GetTypes()
+                    //..that implements ICommand.
+                    .Where(t => t.GetInterfaces()
+                        .Any(i => i.Equals(typeof(ICommand)))));
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Commandr.Commandr"/> with the given commands.
+        /// </summary>
+        /// <param name="commands">A collection of the types of the command classes that should be registered.</param>
+        public Commandr(IEnumerable<_Type> commands)
+        {
+            this.commands = commands;
         }
     }
 }
